92502703209048b3bebede485ce6b05969274a9d
Emily Ly <em.a.ly63@gmail.com>
2021-09-07 23:13:56 -0400

    putting it together assignment - state added

--- react/prop_it_up/src/components/PersonCard.js
+++ react/prop_it_up/src/components/PersonCard.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ageNow: this.props.age
+        };
+    }
+
+
     render() {
+        const addBirthday = () => {
+            this.setState({ageNow: this.state.ageNow+1});
+        }
+
+
         return (
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.ageNow}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={addBirthday}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
