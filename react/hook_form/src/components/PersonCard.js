a371c832931aff15734e648812e4fa0d8b39b0b7
Emily Ly <em.a.ly63@gmail.com>
2021-09-09 00:23:42 -0400

    more forms assignment uploaded - validations added

--- react/hook_form/src/components/PersonCard.js
+++ react/hook_form/src/components/PersonCard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = () => {
     const [firstName, setFirstName] = useState("");
@@ -13,23 +13,51 @@ const PersonCard = () => {
             <form>
                 <div className="form-group">
                     <label htmlFor="firstName">First Name:</label>
-                    <input onChange={(e) => setFirstName(e.target.value)} type="text" name="firstName" className="form-control"/>
+                    <input onChange={(e) => setFirstName(e.target.value)} type="text" name="firstName" className="form-control" />
+                    {firstName.length < 2 && firstName.length != 0 ?
+                        <p className="text-danger">First name must be at least 2 characters</p> :
+                        ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="lastName">Last Name:</label>
-                    <input onChange={(e) => setLastName(e.target.value)} type="text" name="lastName" className="form-control"/>
+                    <input onChange={(e) => setLastName(e.target.value)} type="text" name="lastName" className="form-control" />
+                    {lastName.length < 2 && lastName.length != 0 ?
+                        <p className="text-danger">Last name must be at least 2 characters</p> :
+                        ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
-                    <input onChange={(e) => setEmail(e.target.value)} type="text" name="email" className="form-control"/>
+                    <input onChange={(e) => setEmail(e.target.value)} type="text" name="email" className="form-control" />
+                    {email.length < 5 && email.length != 0 ?
+                        <p className="text-danger">Email must be at least 5 characters</p> :
+                        ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" className="form-control"/>
+                    <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" className="form-control" />
+                    {password.length < 8 && password.length != 0 ?
+                        <p className="text-danger">Password must be at least 8 characters</p> :
+                        ""
+                    }
+                    {password != confirmPassword ?
+                        <p className="text-danger">Passwords do not match</p> :
+                        ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="confirmPassword">Confirm Password:</label>
-                    <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" name="confirmPassword" className="form-control"/>
+                    <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" name="confirmPassword" className="form-control" />
+                    {confirmPassword.length < 8 && confirmPassword.length != 0 ?
+                        <p className="text-danger">Password must be at least 8 characters</p> :
+                        ""
+                    }
+                    {password != confirmPassword ?
+                        <p className="text-danger">Passwords do not match</p> :
+                        ""
+                    }
                 </div>
             </form>
             <br />
