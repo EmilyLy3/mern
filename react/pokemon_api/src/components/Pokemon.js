736b856d403b1fb955477ccc22e15604094fd384
Emily Ly <em.a.ly63@gmail.com>
2021-09-14 04:32:12 -0400

    pokemon api assignment uploaded

---
+++ react/pokemon_api/src/components/Pokemon.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+
+const Pokemon = () => {
+
+    const [allPokemon, setAllPokemon] = useState([]);
+
+
+    const clickHandler = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=1200")
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                // console.log("--------------");
+                // console.log(response);
+                setAllPokemon(response.results);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        console.log(allPokemon);
+    };
+
+
+    return (
+        <div>
+            <h1>Welcome to the Pokemon API!</h1>
+            <button onClick={clickHandler}>Fetch Pokemon</button>
+            {
+                allPokemon.map((pokemon, index) => {
+                    return <p key={index}>{index+1}. {pokemon.name}</p>
+                })
+            }
+        </div>
+    );
+};
+
+export default Pokemon;
